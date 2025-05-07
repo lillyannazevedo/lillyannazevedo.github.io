de0bcd98740f39cfb121d1e9ecd82574eb8f6ea9
Lillyanna Azevedo <lillyannazevedo@aol.com>
2025-05-07 09:56:47 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -127,11 +127,7 @@ const CONFIG = {
       journalName: 'Microbiology Resource Announcement',
       authors: 'Lillyanna Azevedo, Julian Garcia, Priscilla Garcia, Taylor Hedley, Tricia Van Laar',
       link: 'https://github.com/lillyannazevedo/lillyannazevedo.github.io/raw/main/MRA_Paper.pdf',
-      description: 'In preparation: Isolated from a Russian space station, Elizabethkingia miricola DSM 14571 is a
-Gram-negative opportunistic and antibiotic resistant human pathogen. Sequencing its genome
-reveals 4,064 genes, including virulence factors and secondary metabolic clusters. This research
-could offer further insight into pathogenicity, environmental resilience, resistance mechanisms,
-and zoonotic risks.',
+      description: 'In preparation: Isolated from a Russian space station, Elizabethkingia miricola DSM 14571 is a Gram-negative opportunistic and antibiotic resistant human pathogen. Sequencing its genome reveals 4,064 genes, including virulence factors and secondary metabolic clusters. This research could offer further insight into pathogenicity, environmental resilience, resistance mechanisms, and zoonotic risks.',
     },
     {
   // Display articles from your medium or dev account. (Optional)
